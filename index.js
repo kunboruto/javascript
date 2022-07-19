7c40439c32b6f624b6d9c9685dcdf4792fd79e0f
kunboruto <kunboruto6@gmail.com>
2022-07-19 14:33:35 +0700

    7 Mengenal Var, Let, dan Const

---
+++ index.js
@@ -0,0 +1,18 @@
+// Global
+var message = "Hello Coding";
+
+// Local
+let msg1 = "Hallo";
+
+// Konstan / Tetap
+const msg2 = "Coding";
+
+message = "Rubah 1";
+
+if (true) {
+    var message = 1;
+    let msg1 = 2;
+}
+
+console.log(message);
+console.log(msg1);
