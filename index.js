50f8cec28da5ca6b1c1f33996e5576a166cab62e
kunboruto <kunboruto6@gmail.com>
2022-07-19 15:07:37 +0700

    Membuat Variabel Tanpa Perintah Var

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-// // Global
-// var message = "Hello Coding";
-
-// // Local
-// let msg1 = "Hallo";
-
-// // Konstan / Tetap
-// const msg2 = "Coding";
-
-// msg3 = "Selamat";
-
-// message = "Rubah 1";
-
-// if (true) {
-//     var message = 1;
-//     let msg1 = 2;
-// }
-
 var message;
 msg1;
 
 console.log(message);
 console.log(msg1);
-// console.log(msg3);
-
 
 // Jika var dideclare tanpa value maka default valuenya = undefined
 // Jika membuat variabel tanpa perintah var maka valuenya harus di set
