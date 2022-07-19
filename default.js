ccce217ed3632f10579aec86aafb21d36da45822
kunboruto <kunboruto6@gmail.com>
2022-07-19 16:02:52 +0700

    Argument / Parameter Default Pada  Function

---
+++ default.js
@@ -0,0 +1,6 @@
+function mintadata(x = 10) {
+    console.log(x);
+}
+
+mintadata(20);
+mintadata();
---
+++ parameter.js
@@ -0,0 +1,8 @@
+function mintadata(x, y, z) {
+    console.log(x);
+    console.log(y);
+    console.log(z);
+    console.log(typeof (z));
+}
+
+mintadata("apakabar", "sehat", "");
