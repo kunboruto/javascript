dc04e9dbb492a8dab79a2db945032bd07fb2c40d
kunboruto <kunboruto6@gmail.com>
2022-07-19 17:04:58 +0700

    Nested IF ELSE

---
+++ nested-if.js
@@ -0,0 +1,18 @@
+var x = 10;
+var y = 20;
+var z = 30;
+
+if (x > y) {
+    if (x > z) {
+        console.log("X adalah yang terbesar")
+    } else {
+        console.log("X adalah yang terbesar kedua")
+
+    }
+} else {
+    if (x < z) {
+        console.log("X adalah yang terkecil")
+    } else {
+        console.log("X adalah yang terkecil kedua")
+    }
+}
