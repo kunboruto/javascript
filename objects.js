7168bbcbd37390a0d78f5cc5ee1db654718fb972
kunboruto <kunboruto6@gmail.com>
2022-07-20 10:31:21 +0700

    Array Dalam Object

--- objects.js
+++ objects.js
@@ -3,9 +3,11 @@ function panggilObjects() {
         type: "Sedan",
         harga: 2000000000,
         warna: "Putih",
+        tahun: [2019, 2020, 2021, 2022]
     }
     console.log(mobil);
     console.log(mobil.warna);
+    console.log(mobil.tahun[0]);
 }
 
 panggilObjects()
