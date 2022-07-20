337e24008cd55d0fbd640381200cb2bdd9b77c4b
kunboruto <kunboruto6@gmail.com>
2022-07-20 09:59:54 +0700

    Perintah Break Dan Continue Pada Perulangan

---
+++ break.js
@@ -0,0 +1,14 @@
+for (var i = 0; i <= 10; i++) {
+    if (i == 6) {
+        continue;
+    }
+
+    if (i == 9) {
+        break;
+    }
+    console.log(i);
+}
+
+
+// break = menghentikan loop
+// continue = skip loop sesuai kondisi
