89f796770a2133660ec704b6bfc4ee749bb3001f
kunboruto <kunboruto6@gmail.com>
2022-07-20 09:33:30 +0700

    Perulangan DO WHILE

---
+++ do-while.js
@@ -0,0 +1,10 @@
+var i = 10;
+
+do {
+    console.log("ini yang ke ", i);
+    i++;
+} while (i < 10);
+
+// Perbedaan WHILE dan DO WHILE
+// DO WHILE akan mengeksekusi statement sebanyak sekali baru mengecek kondisi.
+// WHILE akan mengecek kondisi baru mengeksekusi statement.
