05610d5aae70ed5fb1b920110e14a6ce50dc4cf5
kunboruto <kunboruto6@gmail.com>
2022-07-20 10:07:13 +0700

    Membuat Array Bercabang

---
+++ nested-array.js
@@ -0,0 +1,11 @@
+function panggilNestedArray() {
+    var dataNama = [["AB", "AC", "AD"], ["Bola1", "Bola2", "Bola3"], ["Rem1", "Rem2", "Rem3"]];
+
+    console.log(dataNama.length);
+    console.log(dataNama[0][0]);
+    console.log(dataNama[1][0]);
+    console.log(dataNama[2][0]);
+
+}
+
+panggilNestedArray();
