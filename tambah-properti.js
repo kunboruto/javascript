9a58764a212f0956d54ff4454b5f77e994c91b54
kunboruto <kunboruto6@gmail.com>
2022-07-20 10:43:38 +0700

    Menambah Object Property

---
+++ tambah-properti.js
@@ -0,0 +1,13 @@
+function panggilObjects() {
+    var mobil = {
+        type: "Sedan",
+        harga: 200000000,
+        warna: "Putih",
+    }
+    mobil.tahun = 2018;
+    mobil.harga = 100000000;
+    console.log(mobil);
+    console.log(mobil.warna);
+}
+
+panggilObjects()
